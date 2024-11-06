357a1386fa49cfa5fcc6face7ab897c6cdc35e43
emilio loeza <101156367+emilioloeza1@users.noreply.github.com>
2024-11-05 20:15:45 -0500

    Create Practica1.php

---
+++ Practica1.php
@@ -0,0 +1,19 @@
+<DOCTYPE html>
+<html>
+<head> <title> Práctica 1 </title>
+</head>
+<body>
+<?php
+ECHO "Hola! Este es mi primer script en PHP <br>";
+//Esto es una línea de comentario
+#Esto también
+/*Esto es un bloque de más de una línea de comentarios*/
+
+$vartex="Hola! Ahora estoy usando variables <br>";
+ECHO $vartex;
+$varnum1=5;
+$varnum2=6;
+$Res=$varnum1+$varnum2;
+ECHO "El resultado de la suma de $varnum1 y $varnum2 es= $Res"; ?>
+</body>
+</html>
