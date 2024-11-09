a26a4b312bcfffb3ad7049852170431706ea158d
emilio loeza <101156367+emilioloeza1@users.noreply.github.com>
2024-11-08 19:26:19 -0500

    Create Practica2.php

---
+++ Practica2.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+<html>
+  <head> <title> Práctica 2 </title> </head>
+  <body>
+    <?php
+$a=5; // variable global
+function variable() {
+  $b=10;
+  ECHO "Esta es una prueba de funciones <br>";
+  ECHO "La variable a es $a <br>";
+  ECHO "La vaeiable b es $b <br>";
+}
+
+ECHO "Esta es una prueba de variables <br>";
+ECHO "La variable a es $a <br>";
+ECHO "La variable b es $b <br>";
+?>
+  </body>
+    
+</html>
