cd4e6bf3c6a13a9311a62dfd812d9cb7531fb340
emilio loeza <101156367+emilioloeza1@users.noreply.github.com>
2024-11-11 15:59:56 -0500

    Create Practica5.php

---
+++ Practica5.php
@@ -0,0 +1,26 @@
+<DOCTYPE html>
+<html>
+<head> <title> Práctica 5 </title> </head>
+<?php
+$dia ="jueves";
+switch ($dia) {
+case "lunes":
+echo "hoy es lunes";
+break;
+case "martes":
+echo "hoy es martes";
+break;
+case "miercoles":
+echo "hoy es miercoles";
+break;
+case "jueves":
+echo "hoy es jueves";
+break;
+case "viernes":
+echo "hoy es viernes";
+break;
+default:
+}
+?>
+</body>
+</html>
