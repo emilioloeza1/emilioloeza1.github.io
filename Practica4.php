1afb39ef64e29ac570ac52bc8106bda8b5a1af5f
emilio loeza <101156367+emilioloeza1@users.noreply.github.com>
2024-11-11 15:54:34 -0500

    Create Practica4.php

---
+++ Practica4.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+<html>
+<head> <title> Práctica 4 </title> </head>
+<body>
+<?php
+$numero=15;
+// verificar si el numero es par o impar 
+if ($numero % 2 ==0) {
+    echo "El numero $numero es par.";
+} else {
+    echo "El numero $numero es impar.";
+}
+?>
+</body>
+</html>
